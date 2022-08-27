4ce356fbb0a4b25c39c04b6b86cee79fbe51ec90
julia-pysmenna <pysmenna@gmail.com>
2022-08-27 13:33:17 +0300

    Add time to js

--- src/app.js
+++ src/app.js
@@ -1,3 +1,26 @@
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes};`;
+  }
+  return `${day} ${hours}:${minutes}`;
+}
+
 function displayTemperature(response) {
   temperatureElement = document.querySelector("#temperature");
   cityElement = document.querySelector("#city");
@@ -5,12 +28,15 @@ function displayTemperature(response) {
   humidityElement = document.querySelector("#humidity");
   pressureElement = document.querySelector("#pressure");
   windElement = document.querySelector("#wind");
+  dateElement = document.querySelector("#date");
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
   descriptionElement.innerHTML = response.data.weather[0].description;
   humidityElement.innerHTML = response.data.main.humidity;
   pressureElement.innerHTML = response.data.main.pressure;
   windElement.innerHTML = Math.round(response.data.wind.speed);
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
+  console.log(response.data);
 }
 
 let city = "Kyiv";
