6ca1baa6a4b5f0311f31a0e25a17af3b1e37bf37
julia-pysmenna <pysmenna@gmail.com>
2022-08-27 15:16:20 +0300

    Set icon and alt from API

--- src/app.js
+++ src/app.js
@@ -29,6 +29,8 @@ function displayTemperature(response) {
   pressureElement = document.querySelector("#pressure");
   windElement = document.querySelector("#wind");
   dateElement = document.querySelector("#date");
+  iconElement = document.querySelector("#icon");
+
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
   descriptionElement.innerHTML = response.data.weather[0].description;
@@ -36,6 +38,11 @@ function displayTemperature(response) {
   pressureElement.innerHTML = response.data.main.pressure;
   windElement.innerHTML = Math.round(response.data.wind.speed);
   dateElement.innerHTML = formatDate(response.data.dt * 1000);
+  iconElement.setAttribute(
+    "src",
+    `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
   console.log(response.data);
 }
 
